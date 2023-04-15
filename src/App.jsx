75b1dbda5c8d15fa87353a6670021f74d4c2a877
DaisyEverard <msdaisyeverard@gmail.com>
2023-04-15 11:47:32 +0100

    FIX: capitals in file paths

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Header from "./common/Header"
 import Footer from "./common/Footer"
-import Home from "./pages/home";
-import Gallery from "./pages/gallery";
-import CV from "./pages/cv";
-import Contact from "./pages/contact";
+import Home from "./pages/Home";
+import Gallery from "./pages/Gallery";
+import CV from "./pages/CV";
+import Contact from "./pages/Contact";
 
 const router = (
   <Routes>
